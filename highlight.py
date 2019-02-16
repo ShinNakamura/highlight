739d56b3b2d4cc40d6cfc7c5c1043626ac957581
Shin Nakamura <sn@i.basicwerk.com>
2019-02-16 20:41:35 +0900

    refactoring highlight.py

--- highlight.py
+++ highlight.py
@@ -6,19 +6,21 @@
 
 import sys
 from functools import reduce
+import html
+
+def readfile_as_list(fname):
+    '''
+    just read a file. return lines as list.
+    '''
+    with open(fname, "rt", encoding="utf-8") as f:
+        for line in f:
+            yield line
 
 def readfile(fname):
     '''
-    just read file. one file -> one string
+    just read a file. one file -> one string
     '''
-    with open(fname, "rt", encoding="utf-8") as f:
-        return "".join([line for line in f])
-
-def escape_html(html):
-    '''
-    escape html tag
-    '''
-    return html.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
+    return "".join(readfile_as_list(fname))
 
 def highlight_kw(src, kw):
     return src.replace(kw, '<span class="highlight">{}</span>'.format(kw))
@@ -27,8 +29,8 @@ def main():
     _, output_template_fname, src_fname, kws_fname = sys.argv
 
     output_template = readfile(output_template_fname)
-    src = escape_html(readfile(src_fname)).replace("\n", "<br />")
-    kws = (escape_html(kw) for kw in (kw.strip() for kw in readfile(kws_fname).split("\n")) if kw)
+    src = html.escape(readfile(src_fname)).replace("\n", "<br />")
+    kws = (html.escape(kw) for kw in (kw.strip() for kw in readfile_as_list(kws_fname)) if kw)
     dst = reduce(highlight_kw, kws, src)
 
     # title and article -- as place holder -- are in OUTPUT template.
